0a01dfcb2f86eec45b6353b0078a6bafbe449200
AMUKELANI465 <mabasaamu0@gmail.com>
2025-06-28 19:55:49 +0200

    update
    i messed up the whole project but managed to fix it again , i deleted all the folder that ia dded in oeder to put the app in the play store

--- android/app/src/main/java/com/amukelani/first_year_guide/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.amukelani.first_year_guide;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- www/android/app/src/main/java/com/amukelani/tvetguide/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.amukelani.tvetguide;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
