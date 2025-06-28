b167b6e2fbc16199226c88509adbf45793e50016
AMUKELANI465 <mabasaamu0@gmail.com>
2025-06-28 19:38:09 +0200

    update

---
+++ android/app/src/main/java/com/amukelani/first_year_guide/MainActivity.java
@@ -0,0 +1,5 @@
+package com.amukelani.first_year_guide;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/android/app/src/main/java/com/amukelani/tvetguide/MainActivity.java
@@ -0,0 +1,5 @@
+package com.amukelani.tvetguide;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
